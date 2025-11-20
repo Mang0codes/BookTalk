30bfc65be38549c8831ab3dbd9d7402d2d4e01c9
Albia Hossain <albiahossain0@gmail.com>
2025-11-21 01:23:40 +0530

    x commit

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-     origin: 'http://localhost:5173',
+     origin: 'https://booktalk-c4q7.onrender.com/',
      credentials: true,
 }));
 app.use(express.json());
